a05e20dfd680e269c2b282ea99ca01ae93d36fc7
Justice League <Justice League justice.league@comics.com>
2024-03-13 11:21:58 +0000

    Add call_script for testing purposes

---
+++ script/call_script.py
@@ -0,0 +1,10 @@
+import os
+from script import ask_chat
+
+prompt = "Mijn vrouw slaat mij, wat kan ik doen ?"
+# prompt = "Ma femme me bat, qu'est-ce que je peux faire ?"
+# question = "My wife is beating me, what could I do?"
+
+print(prompt)
+
+print(ask_chat(prompt))
