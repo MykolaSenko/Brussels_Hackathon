519ea08503d43a85ca02a1745f22720879ea6d74
Justice League <Justice League justice.league@comics.com>
2024-03-13 11:04:58 +0000

    Solve merge conflicts

--- script/script.py
+++ script/script.py
@@ -1,7 +1,43 @@
 import streamlit as st
 
+from haystack import Pipeline
+from haystack.components.fetchers import LinkContentFetcher
+from haystack.components.converters import HTMLToDocument
+from haystack.components.builders import PromptBuilder
+from haystack.components.generators import OpenAIGenerator
 
 def ask_chat(prompt):
+    fetcher = LinkContentFetcher()
+    converter = HTMLToDocument()
+    prompt_template = """
+    Please help me know what to do.
+    I'm in Belgium
 
-    response = f"Tell me more about: {prompt}"
-    return response
+
+    My question is: {{query}}
+    Please answer in the language of the question.
+    """
+
+    # Only use this information:
+
+    # {% for document in documents %}
+    # {{document.content}}
+    # {% endfor %}
+
+    prompt_builder = PromptBuilder(template=prompt_template)
+    llm = OpenAIGenerator()
+
+    pipeline = Pipeline()
+    pipeline.add_component("fetcher", fetcher)
+    pipeline.add_component("converter", converter)
+    pipeline.add_component("prompt", prompt_builder)
+    pipeline.add_component("llm", llm)
+
+    pipeline.connect("fetcher.streams", "converter.sources")
+    # pipeline.connect("converter.documents", "prompt.documents")
+    pipeline.connect("prompt.prompt", "llm.prompt")
+
+    result = pipeline.run({"fetcher": {"urls": ["https://resourcehub.bakermckenzie.com/en/resources/fighting-domestic-violence/europe/belgium/topics/4-protection-for-domestic-violence-victims-and-relief-granted"]},
+                "prompt": {"query": prompt}})
+    
+    return result["llm"]["replies"][0]
