ac3c71df149728ba824c320d3dc4aef762f124a9
Thomas Thiry <thomas.thiry@gmail.com>
2024-03-14 12:23:38 +0100

    Avoid sending > to the api

--- script/call_script.py
+++ script/call_script.py
@@ -3,28 +3,28 @@ from script_chat import ask_chat
 
 message_history = []
 
-prompt = "> Hello, I need help"
-print(prompt)
+prompt = "Hello, I need help"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> Yes"
-print(prompt)
+prompt = "Yes"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> No"
-print(prompt)
+prompt = "No"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> My wife beats me all the time"
-print(prompt)
+prompt = "My wife beats me all the time"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> Since marriage, 2 years ago. She beats me every day in the morning before leaving to work."
-print(prompt)
+prompt = "Since marriage, 2 years ago. She beats me every day in the morning before leaving to work."
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
@@ -34,13 +34,13 @@ print("[System] " + prompt)
 response, _ = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> Yes, I even went to the hospital last week!"
-print(prompt)
+prompt = "Yes, I even went to the hospital last week!"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "> Well, I have the hospital bills"
-print(prompt)
+prompt = "Well, I have the hospital bills"
+print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
