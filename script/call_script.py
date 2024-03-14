30401d4352a497d6a3ace56eed9fbeb0f345f808
Thomas Thiry <thomas.thiry@gmail.com>
2024-03-14 12:17:42 +0100

    Ask status at every step

--- script/call_script.py
+++ script/call_script.py
@@ -2,34 +2,7 @@ import os
 from script_chat import ask_chat
 
 message_history = []
-
-prompt = "Hello, I need help"
-print("> " + prompt)
-response, message_history = ask_chat(prompt, message_history)
-print(response + "\n")
-
-prompt = "Yes"
-print("> " + prompt)
-response, message_history = ask_chat(prompt, message_history)
-print(response + "\n")
-
-prompt = "No"
-print("> " + prompt)
-response, message_history = ask_chat(prompt, message_history)
-print(response + "\n")
-
-prompt = "My wife beats me all the time"
-print("> " + prompt)
-response, message_history = ask_chat(prompt, message_history)
-print(response + "\n")
-
-prompt = "Since marriage, 2 years ago. She beats me every day in the morning before leaving to work."
-print("> " + prompt)
-response, message_history = ask_chat(prompt, message_history)
-print(response + "\n")
-
-#### SYSTEM ####
-prompt = """
+status_prompt = """
         Please fill this form with confirmed information about the user that you are sure about based on the chat so far. Answer each element with only 'yes', 'no' or 'not sure yet'.
         For example
         - Is the moon full?: not sure yet
@@ -40,8 +13,55 @@ prompt = """
         - Is the user married?: 
         - Is the user a child?: 
         """
-print("[System] " + prompt)
-response, _ = ask_chat(prompt, message_history)
+
+prompt = "Hello, I need help"
+print("> " + prompt)
+response, message_history = ask_chat(prompt, message_history)
+print(response + "\n")
+
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
+
+prompt = "> Yes"
+print(prompt)
+response, message_history = ask_chat(prompt, message_history)
+print(response + "\n")
+
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
+
+prompt = "> No"
+print(prompt)
+response, message_history = ask_chat(prompt, message_history)
+print(response + "\n")
+
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
+
+prompt = "> My wife beats me all the time"
+print(prompt)
+response, message_history = ask_chat(prompt, message_history)
+print(response + "\n")
+
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
+
+prompt = "> Since marriage, 2 years ago. She beats me every day in the morning before leaving to work."
+print(prompt)
+response, message_history = ask_chat(prompt, message_history)
+print(response + "\n")
+
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
 print(response + "\n")
 
 prompt = "Yes, I even went to the hospital last week!"
@@ -49,9 +69,17 @@ print("> " + prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-prompt = "Well, I have the hospital bills"
-print("> " + prompt)
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
+
+prompt = "> Well, I have the hospital bills"
+print(prompt)
 response, message_history = ask_chat(prompt, message_history)
 print(response + "\n")
 
-
+#### SYSTEM ####
+response, _ = ask_chat(status_prompt, message_history)
+print("[System]")
+print(response + "\n")
